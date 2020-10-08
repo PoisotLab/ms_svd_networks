e32cf3ecc297c5db5e7e6bcd3eacdb2f8ea493d6
Tanya Strydom <tanya.strydom@icloud.com>
2020-10-08 10:56:57 +0200

    🐝 bee swarm plot and changed dims for resilience figures

--- code/main.jl
+++ code/main.jl
@@ -42,30 +42,28 @@ Outputs = @from i in DataFrame(Richness = richness.(Bs),
 draw(PNG(joinpath("figures", "interactiontype_v_rank&entropy.png"), 15cm, 20cm, dpi=300),
     plot(stack(Outputs, [:RankDefficiencyRel, :Entropy], variable_name =:measure, value_name=:value),
         x=:InteractionType, y=:value,
-        color=:InteractionType, ygroup =:measure, Geom.subplot_grid(Geom.boxplot, free_y_axis=true),
-        Guide.xlabel("Interaction Type"), Guide.ylabel(nothing)))
+        color=:InteractionType, ygroup =:measure,
+        Geom.subplot_grid(Geom.beeswarm(orientation=:vertical, padding=0.1mm), free_y_axis=true),
+        alpha = [0.3], Guide.xlabel("Interaction Type"), Guide.ylabel(nothing)))
+
+## Here we could plot Entropy and various measures of Rank
+
+draw(PNG(joinpath("figures", "entropy_v_rank.png"), 20cm, 20cm, dpi = 300),
+plot(Outputs,
+    x=:RankDefficiencyRel, y=:Entropy,
+    color=:InteractionType, alpha = [0.6], Guide.xlabel("Relative rank defficiency"), Guide.ylabel("Entropy")))
 
 ## Size vs Rank & Entropy
 
-draw(PNG(joinpath("figures", "size_v_rank.png"), 15cm, 20cm, dpi=300),
+draw(PNG(joinpath("figures", "size_v_rank&entropy.png"), 15cm, 20cm, dpi=300),
     plot(stack(Outputs, [:RankDefficiencyRel, :Entropy], variable_name =:measure, value_name=:value),
         x=:Richness, y=:value,
         color=:InteractionType, ygroup =:measure, Geom.subplot_grid(Geom.point, free_y_axis=true),
         alpha = [0.6], Guide.xlabel("Richness"), Guide.ylabel(nothing)))
 
-## Here we could plot Entropy and various measures of Rank
-
-#=
-draw(PNG(joinpath("figures", "entropy_v_rank.png"), 20cm, 20cm, dpi = 300),
-    vstack(hstack(plot(Outputs, x=:Richness, y=[:RankDefficiencyRel, :Entropy], Geom.point),
-                plot(Outputs, x=:Rank, y=:Entropy, Geom.point)),
-            hstack(plot(Outputs, x=:RankDefficiency, y=:Entropy, Geom.point, Guide.xlabel("Rank Defficiency")),
-                plot(Outputs, x=:RankDefficiencyRel, y=:Entropy, Geom.point, Guide.xlabel("Rank Defficiency (relative)")))))
-=#
-
 ## Other measures of networks vs Rank & Entropy
 
-draw(PNG(joinpath("figures", "entropy_v_others.png"), 20cm, 20cm, dpi = 300),
+draw(PNG(joinpath("figures", "others_v_rank&entropy.png"), 20cm, 20cm, dpi = 300),
     plot(stack(stack(Outputs, [:RankDefficiencyRel, :Entropy], [:Nestedness, :SpectralRadiance, :InteractionType],
         variable_name =:measure, value_name=:value), [:Nestedness, :SpectralRadiance],
         variable_name =:method, value_name=:metric),
@@ -82,12 +80,12 @@ f_decreasing = _order_species_for_removal(false);
 AUC = DataFrame(Random = [extinction_robustness(extinction(B, f_random)) for B in Bs],
                 Increasing = [extinction_robustness(extinction(B, f_increasing)) for B in Bs],
                 Decreasing = [extinction_robustness(extinction(B, f_decreasing)) for B in Bs],
-                Random_1 = [extinction_robustness(extinction(B, f_random, dims = 1)) for B in Bs],
-                Increasing_1 = [extinction_robustness(extinction(B, f_increasing, dims = 1)) for B in Bs],
-                Decreasing_1 = [extinction_robustness(extinction(B, f_decreasing, dims = 1)) for B in Bs],
-                Random_2 = [extinction_robustness(extinction(B, f_random, dims = 2)) for B in Bs],
-                Increasing_2 = [extinction_robustness(extinction(B, f_increasing, dims = 2)) for B in Bs],
-                Decreasing_2 = [extinction_robustness(extinction(B, f_decreasing, dims = 2)) for B in Bs],
+                Random_1 = [extinction_robustness(extinction(B, f_random, dims = 1), dims = 2) for B in Bs],
+                Increasing_1 = [extinction_robustness(extinction(B, f_increasing, dims = 1), dims = 2) for B in Bs],
+                Decreasing_1 = [extinction_robustness(extinction(B, f_decreasing, dims = 1), dims = 2) for B in Bs],
+                Random_2 = [extinction_robustness(extinction(B, f_random, dims = 2), dims = 1) for B in Bs],
+                Increasing_2 = [extinction_robustness(extinction(B, f_increasing, dims = 2), dims = 1) for B in Bs],
+                Decreasing_2 = [extinction_robustness(extinction(B, f_decreasing, dims = 2), dims = 1) for B in Bs],
                 Rank = ((maxrank.(Bs) .- rank.(Bs)) ./ maxrank.(Bs)),
                 Entropy = svd_entropy.(Bs),
                 InteractionType = [y[:Type_of_interactions] for y in web_of_life()])
